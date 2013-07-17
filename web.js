41c8fd06fd5d8a02a7c393ac97c592f2404e82b9
Aardwulf <radek888401@gmail.com>
2013-07-17 13:57:20 +0000

    revert

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require('fs');
-// var app = express.createServer(express.logger());
-var app = express();
+var app = express.createServer(express.logger());
+
 
 var filer = fs.readFileSync('index.html');
 // var buf = new Buffer(256);
